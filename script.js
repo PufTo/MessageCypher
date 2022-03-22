88a1c1985de09e6e4299bc893bd61c1cc64c9fd3
Paul-Todea Nicodin <50740745+PufTo@users.noreply.github.com>
2022-03-22 15:49:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const encryptedMessageBox = document.getElementsByClassName("encryptedMessage")[
 
 const validCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",  "S", "T", "U", "V",  "W", "X", "Y", "Z",  "0", "1",  "2", "3", "4", "5", "6", "7",  "8", "9", ".", ",", "?", "!", "'", "_", "-", "&", "@", "#", "$", "%", "*", "(", ")", " "];
 
-console.log(messageInput, secretCodeInput, encryptButton, decryptButton, copyButton);
-
 const validateMessageAndKey = (message, key) => {
     try {
         console.log(message.split(''));
@@ -60,8 +58,8 @@ const decryptMessage = (event)=>{
         const keyCharacter = key[index%key.length];
         const keyIndex = validCharacters.indexOf(keyCharacter);
         let decryptedIndex = messageIndex - keyIndex;
-        if (descryptedIndex < 0)
-            decryptedIndexFinal += validCharacters.length;
+        if (decryptedIndex < 0)
+            decryptedIndex += validCharacters.length;
         return validCharacters[decryptedIndex];
     } );
     console.log(encryptedMessage.join(""));
@@ -74,7 +72,6 @@ encryptButton.addEventListener("click", encryptMessage);
 decryptButton.addEventListener("click", decryptMessage);
 
 copyButton.addEventListener("click", (event)=> {
-    // FOLOSIM SI NOI ASYNC???
     event.preventDefault();
     const newClipboardText = encryptedMessageBox.value;
     navigator.clipboard.writeText(newClipboardText).then();
