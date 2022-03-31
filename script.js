157d077177ed651237532700dd003d6e8d38c4d8
Paul-Todea Nicodin <50740745+PufTo@users.noreply.github.com>
2022-03-31 20:52:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,144 @@
-'use strict';
+"use strict";
 console.log("hello");
-// ce i cu selecturile astea ALOO
-const messageInput = document.getElementsByClassName("messageToEncrypt")[0];
-const secretCodeInput = document.getElementsByClassName("secretCode")[0];
-const encryptButton = document.getElementsByClassName("button-encrypt")[0];
-const decryptButton = document.getElementsByClassName("button-decrypt")[0];
-const copyButton = document.getElementsByClassName("button-copyMessage")[0];
-const encryptedMessageBox = document.getElementsByClassName("encryptedMessage")[0];
 
-const validCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",  "S", "T", "U", "V",  "W", "X", "Y", "Z",  "0", "1",  "2", "3", "4", "5", "6", "7",  "8", "9", ".", ",", "?", "!", "'", "_", "-", "&", "@", "#", "$", "%", "*", "(", ")", " "];
+const messageInput = document.getElementById("cypher-message-input");
+const secretCodeInput = document.getElementById("cypher-key-input");
+const encryptedMessageBox = document.getElementById("cypher-result-text");
+
+const encryptButton = document.querySelector(".button-encrypt");
+const decryptButton = document.querySelector(".button-decrypt");
+const swapButton = document.querySelector(".button-swap");
+const copyButton = document.querySelector(".button-copy");
+
+const validCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  ".",
+  ",",
+  "?",
+  "!",
+  "'",
+  "_",
+  "-",
+  "&",
+  "@",
+  "#",
+  "$",
+  "%",
+  "*",
+  "(",
+  ")",
+  " ",
+];
 
 const validateMessageAndKey = (message, key) => {
-    try {
-        console.log(message.split(''));
-        message.split('').forEach(element => {
-            if(!validCharacters.includes(element))
-                throw "The message contains invalid characters!";
-        });
-        key.split('').forEach(element => {
-            if(!validCharacters.includes(element))
-                throw "The key contains invalid characters!";
-        });
-      }
-      catch(err) {
-        alert (err);
-        //message.innerHTML = "Eroare";
-      }
-}
+  try {
+    console.log(message.split(""));
+    message.split("").forEach((element) => {
+      if (!validCharacters.includes(element))
+        throw "The message contains invalid characters!";
+    });
+    if (!key) throw "Can't have empty key!";
+    key.split("").forEach((element) => {
+      if (!validCharacters.includes(element))
+        throw "The key contains invalid characters!";
+    });
+  } catch (err) {
+    alert(err);
+    //message.innerHTML = "Eroare";
+    return false;
+  }
+  return true;
+};
 
-const encryptMessage = (event)=>{
-    event.preventDefault();
-    const messageToEncrypt = messageInput.value.toUpperCase();
-    const key = secretCodeInput.value.toUpperCase();
-    validateMessageAndKey(messageToEncrypt, key);
+const encryptMessage = (event) => {
+  event.preventDefault();
+  const messageToEncrypt = messageInput.value.toUpperCase();
+  const key = secretCodeInput.value.toUpperCase();
+  if (!validateMessageAndKey(messageToEncrypt, key)) return;
 
-    const encryptedMessage = messageToEncrypt.split('').map((character, index) => {
-        const messageIndex = validCharacters.indexOf(character);
-        const keyCharacter = key[index%key.length];
-        const keyIndex = validCharacters.indexOf(keyCharacter);
-        //Returns the current encrypted element
-        return validCharacters[(messageIndex + keyIndex)%validCharacters.length];
-    } );
-    console.log(encryptedMessage.join(""));
+  const encryptedMessage = [...messageToEncrypt].map((character, index) => {
+    const messageIndex = validCharacters.indexOf(character);
+    const keyCharacter = key[index % key.length];
+    const keyIndex = validCharacters.indexOf(keyCharacter);
 
-    encryptedMessageBox.value = encryptedMessage.join("").toLowerCase();
-}
+    //Returns the current encrypted element
+    return validCharacters[(messageIndex + keyIndex) % validCharacters.length];
+  });
+  console.log(encryptedMessage.join(""));
 
-const decryptMessage = (event)=>{
-    event.preventDefault();
-    const messageToEncrypt = messageInput.value.toUpperCase();
-    const key = secretCodeInput.value.toUpperCase();
-    validateMessageAndKey(messageToEncrypt, key);
+  encryptedMessageBox.value = encryptedMessage.join("").toLowerCase();
+};
 
+const decryptMessage = (event) => {
+  event.preventDefault();
+  const messageToEncrypt = messageInput.value.toUpperCase();
+  const key = secretCodeInput.value.toUpperCase();
+  if (!validateMessageAndKey(messageToEncrypt, key)) return;
 
-    const encryptedMessage = messageToEncrypt.split('').map((character, index) => {
-        const messageIndex = validCharacters.indexOf(character);
-        const keyCharacter = key[index%key.length];
-        const keyIndex = validCharacters.indexOf(keyCharacter);
-        let decryptedIndex = messageIndex - keyIndex;
-        if (decryptedIndex < 0)
-            decryptedIndex += validCharacters.length;
-        return validCharacters[decryptedIndex];
-    } );
-    console.log(encryptedMessage.join(""));
+  const encryptedMessage = [...messageToEncrypt].map((character, index) => {
+    const messageIndex = validCharacters.indexOf(character);
+    const keyCharacter = key[index % key.length];
+    const keyIndex = validCharacters.indexOf(keyCharacter);
+    let decryptedIndex = messageIndex - keyIndex;
+    if (decryptedIndex < 0) decryptedIndex += validCharacters.length;
 
-    encryptedMessageBox.value = encryptedMessage.join("").toLowerCase();
-}
+    return validCharacters[decryptedIndex];
+  });
+  console.log(encryptedMessage.join(""));
+
+  encryptedMessageBox.value = encryptedMessage.join("").toLowerCase();
+};
+
+const swapMessages = function (event) {
+  event.preventDefault();
+  messageInput.value = encryptedMessageBox.value;
+};
+
+const copyResult = function (event) {
+  event.preventDefault();
+  const newClipboardText = encryptedMessageBox.value;
+  navigator.clipboard.writeText(newClipboardText);
+};
 
 encryptButton.addEventListener("click", encryptMessage);
 
 decryptButton.addEventListener("click", decryptMessage);
 
-copyButton.addEventListener("click", (event)=> {
-    event.preventDefault();
-    const newClipboardText = encryptedMessageBox.value;
-    navigator.clipboard.writeText(newClipboardText).then();
-})
+swapButton.addEventListener("click", swapMessages);
+
+copyButton.addEventListener("click", copyResult);
